84d70bdd8d76d46b0b710b74c51448e90ad7f9a6
syket <syket@genuino.world>
2026-05-10 12:20:11 +0600

    installed the project dependency

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def main():
     print("Hello from rag-with-langchain!")
+    print(os.environ.get("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
